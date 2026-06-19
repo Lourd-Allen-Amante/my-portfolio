710f2c0784d41d53370dd7808e1cf4363c9f45f2
Lourd Allen Amante <lourdallen30@gmail.com>
2026-06-19 11:54:10 +0800

    feat: Addead CTA button and changed descriptions

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -7,13 +7,21 @@ function HeroSection() {
         <section className="heroMid-left">
           <h2 className="heroMid-Intro">Hi! I'm Lourd Allen Amante</h2>
           <span className="highlight">
-            Incoming 3rd Year IT student & Aspiring Back-end Developer
+            <button className="btn-subPrimary">JavaScript</button>
+            <button className="btn-sub">Node.js</button>
+            <button className="btn-sub">Express.js</button>
+            <button className="btn-sub">MySql</button>
+            <button className="btn-sub">Java</button>
           </span>
           <p className="heroMid-desc">
-            IT student from New Era University who have experience building web
-            applications. Currently exploring both frontend and backend
-            development.
+            IT student from New Era University, who has experience building web
+            applications. Currently focused on building REST APIs,
+            database-driven applications, and modern web solutions.
           </p>
+          <div className="btnRow">
+            <button className="btn-primary"> View my projects </button>
+            <button className="btn-secondary"> Contact me </button>
+          </div>
         </section>
         <div className="heroMid-right">
           <img src={photo} />
