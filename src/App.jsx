a807bbf4dc44fc9a6c4703610574f23ade179635
Lourd Allen Amante <lourdallen30@gmail.com>
2026-06-20 22:06:50 +0800

    feat: Added about me section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
+import About from "./components/About";
 import "./App.css";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div>
       <Navbar />
       <HeroSection />
+      <About/>
     </div>
   );
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,31 @@
+function About() {
+  return (
+    <div className="aboutContainer">
+    <div className="aboutText">
+      <h2>About me :3</h2>
+      <p className="about-desc">
+        I’m Lourd Allen G. Amante, a passionate web developer and incoming 3rd
+        year I.T student from New Era University leaning towards back-end
+        development with over 1 year of experience from school and practice.
+      </p>
+      <p className="about-desc">
+        I specialise in creating user-friendly websites. I work with Java,
+        JavaScript, React, HTML, and CSS.
+      </p>
+      <p className="about-desc">
+        I've contributed to group projects like a Customer Management System
+        where I handled QA and documentation. Outside of coding, I'm constantly
+        exploring new things — whether it's picking up new dev skills or working
+        on personal projects that challenge me to grow.
+      </p>
+      </div>
+      <div className="aboutBoxes">
+        <div className="boxContainer"> </div>
+        <div className="boxContainer"> </div>
+        <div className="boxContainer"> </div>
+      </div>
+    </div>
+  );
+}
+
+export default About;
